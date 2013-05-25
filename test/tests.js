2ffbebba61e45b09197073c174bc02cb85b9b3ff
Brent Montague <bigbam505@gmail.com>
2013-05-24 22:52:10 -0400

    Add list units functionality

--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,15 @@
-var should = require('should');
+var should = require('should'),
+    nock = require('nock');
 
 var llib = require('../lib/luckycharms-library.js');
+
+var tOptions = {
+  url: 'http://luckycharms.dev/',
+  api_key: 'apikey'
+}
+
+var Library = llib(tOptions);
+
 
 describe('luckycharms-library', function(){
   it('can be created', function(){
@@ -12,4 +21,20 @@ describe('luckycharms-library', function(){
     should.exist(lib.version);
     lib.version.should.match(/^\d+\.\d+\.\d+$/);
   })
+
+  describe('#ListUnits', function() {
+    nock('http://luckycharms.dev')
+    .get('/admin/units.json?api_key=apikey')
+    .reply(200, [{id: 2}, {id: 1}]);
+
+    it('returns a list of units', function(done){
+      Library.ListUnits(function(data) {
+        data.should.includeEql({id: 2});
+        data.should.includeEql({id: 1});
+        done();
+      });
+    })
+  })
+
 })
+
